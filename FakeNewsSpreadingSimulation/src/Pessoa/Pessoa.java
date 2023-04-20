59d67f9cfbe7b244504c648618658d3856611b9b
brunoandwele <bruno.3a@outlook.com>
2023-04-20 19:20:34 -0300

    Adicionada a funcao de movimentacao das pessoas

--- FakeNewsSpreadingSimulation/src/Pessoa/Pessoa.java
+++ FakeNewsSpreadingSimulation/src/Pessoa/Pessoa.java
@@ -11,30 +11,34 @@ import java.util.Random;
 
 public class Pessoa implements IMovable{
   
-    private int coordenadaAtualX = 30, coordenadaAtualY = 15;
+    private int coordenadaAtualX, coordenadaAtualY;
     private int coordenadaAntigaX, coordenadaAntigaY;
-    
     private int numeroDaCor = 20; //Pessoa sem efeitos (cor default)
-    
+    private static int baseWhatsAppID = 0;
     private String whatsAppID;
+    private ArrayList<String> contatos = new ArrayList<>();
     
-    private ArrayList<String> contatos = new ArrayList<String>();
     
+   
     private int gerarDirecao(){
        //gera a proxima direcao
         
        Random random = new Random();
        int proximaDirecao = random.nextInt(4);
        
-       return proximaDirecao = 2;
+       return proximaDirecao;
         
     }
-    
+    private void setCoordenadasIniciais(){
+        Random random = new Random();
+        coordenadaAtualX = random.nextInt(1,59);
+        coordenadaAtualY = random.nextInt(1,29);
+    }
     private void salvarCoordenadasAnitgas(){
        this.coordenadaAntigaX = this.coordenadaAtualX;
        this.coordenadaAntigaY = this.coordenadaAtualY;
     }
-    
+    @Override
     public void mover(){
         
        salvarCoordenadasAnitgas();
@@ -42,75 +46,80 @@ public class Pessoa implements IMovable{
        int proximaDirecao = gerarDirecao();
        
        switch(proximaDirecao){
-           
-           //Para esquerda
-           case 0:
+           case 0 -> {
                this.coordenadaAtualX -= 1;
                
                if (this.coordenadaAtualX < 1){
                    this.coordenadaAtualX = 58;
                }
-               
-               break;
-               
-           //Para direita
-           case 1:
+            }
+           case 1 -> {
                this.coordenadaAtualX += 1;
                
                if (this.coordenadaAtualX > 58){
                    this.coordenadaAtualX = 1;
                }
-               
-               break;
-               
-           //Para cima
-           case 2:
+            }
+           case 2 -> {
                this.coordenadaAtualY -= 1;
                
                if (this.coordenadaAtualY < 1){
                    this.coordenadaAtualY = 28;
                }
-               
-               break;
-               
-           //Para baixo
-           case 3:
+            }
+           case 3 -> {
                this.coordenadaAtualY += 1;
                
                if (this.coordenadaAtualY > 28){
                    this.coordenadaAtualY = 1;
                }
+            }
                
-               break;
-               
-           default:
+           default -> {
                this.coordenadaAtualX = this.coordenadaAntigaX;
                this.coordenadaAtualY = this.coordenadaAntigaY;
+            }
        }
-       
+        //Para esquerda
+        //Para direita
+        //Para cima
+        //Para baixo
+               
     }
 
+    
+    
+    public Pessoa(){
+        
+        String numeroID = String.valueOf(baseWhatsAppID);
+        this.whatsAppID = "#" + numeroID;
+        
+        baseWhatsAppID++;
+        
+        setCoordenadasIniciais();
+        
+    }
+    
     public int getCoordenadaAtualX() {
         return coordenadaAtualX;
     }
-
     public int getCoordenadaAtualY() {
         return coordenadaAtualY;
     }
-
     public int getCoordenadaAntigaX() {
         return coordenadaAntigaX;
     }
-
     public int getCoordenadaAntigaY() {
         return coordenadaAntigaY;
     }
-
     public int getNumeroDaCor() {
         return numeroDaCor;
     }
+    public String getWhatsAppID() {
+        return whatsAppID;
+    }
+    public ArrayList<String> getContatos() {
+        return contatos;
+    } 
 
-    
-    
-    
 }
