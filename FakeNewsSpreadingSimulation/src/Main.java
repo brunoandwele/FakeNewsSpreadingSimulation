9d7af87577638f8d45250c33bbb06e8c5d1e3779
brunoandwele <bruno.3a@outlook.com>
2023-04-20 19:22:23 -0300

    Adicionei para rodar a funcao que anima o mundo na main.

--- FakeNewsSpreadingSimulation/src/Main.java
+++ FakeNewsSpreadingSimulation/src/Main.java
@@ -11,22 +11,10 @@ public class Main {
 
     
     public static void main(String[] args) {
+        
         Mundo mundo = new Mundo();
+        mundo.animacaoMundo();
         
-        
-        while(true){
-            
-            mundo.desenhaMundoConsole();
-            mundo.atualizaPosicaoPessoas();
-            
-            try{
-                Thread.sleep(300);
-            }
-            catch(Exception e){
-                e.printStackTrace();
-            }
-            
-        }
     }
     
 }
