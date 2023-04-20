2508bfb624ff2e30fbc18cbb9aa6722dd16fbc61
brunoandwele <bruno.3a@outlook.com>
2023-04-20 19:21:29 -0300

    Adicionada a funcao de criar pessoas e colocar na lista, funcao que atualiza os dados das pessaos.

--- FakeNewsSpreadingSimulation/src/Mundo/Mundo.java
+++ FakeNewsSpreadingSimulation/src/Mundo/Mundo.java
@@ -5,6 +5,7 @@
 package Mundo;
 
 import Pessoa.Pessoa;
+import java.util.ArrayList;
 
 /**
  *
@@ -12,38 +13,52 @@ import Pessoa.Pessoa;
  */
 public class Mundo {
     
-    public int[][] mapa = new int[30][60];
+    public int[][] mapaFisico = new int[30][60];
+    public String[][] mapaDados = new String[30][60];
+    private ArrayList<Pessoa> pessoasDoMundo = new ArrayList<>();
     
-    private Pessoa pessoa = new Pessoa();
     
-    public void geraMatrizMundo(){
+    
+    public void gerarMatrizMundo(){
         
         // preenche a primeira e última coluna com 1
         for (int i = 0; i < 30; i++) {
-            mapa[i][0] = 1;
-            mapa[i][59] = 1;
+            mapaFisico[i][0] = 1;
+            mapaFisico[i][59] = 1;
+            mapaDados[i][0] = "1";
+            mapaDados[i][59] = "1";
         }
 
         // preenche a primeira e última linha com 1
         for (int i = 0; i < 60; i++) {
-            mapa[0][i] = 1;
-            mapa[29][i] = 1;
+            mapaFisico[0][i] = 1;
+            mapaFisico[29][i] = 1;
+            mapaDados[0][i] = "1";
+            mapaDados[29][i] = "1";
         }
 
         // preenche o interior da matriz com 0
         for (int i = 1; i < 29; i++) {
             for (int j = 1; j < 59; j++) {
-                mapa[i][j] = 0;
+                mapaFisico[i][j] = 0;
+                mapaDados[i][j] = "0";
             }
         }
     }
-    
+    public void gerarPessoasMundo(){
+        for(int i = 0; i < 100; i++){
+            Pessoa pessoaNova = new Pessoa();
+            pessoasDoMundo.add(pessoaNova);
+            System.out.println("Pessoa add");
+        }
+        System.out.println(pessoasDoMundo.size());
+    }
     public void desenhaMundoConsole(){
         
-        for(int i = 0; i < mapa.length; i++){
-            for(int j = 0; j < mapa[i].length; j++){
+        for(int i = 0; i < mapaFisico.length; i++){
+            for(int j = 0; j < mapaFisico[i].length; j++){
                 
-                switch(mapa[i][j]){
+                switch(mapaFisico[i][j]){
                     case 0:
                         System.out.print(" ");
                         break;
@@ -61,31 +76,58 @@ public class Mundo {
         }
         System.out.println();
     }
-    
-    public void atualizaPosicaoPessoas(){
-        
-        pessoa.mover();
+    public void atualizaPessoasNosMapas(Pessoa pessoa){
         
         int coordenadaAtualX = pessoa.getCoordenadaAtualX();
         int coordenadaAtualY = pessoa.getCoordenadaAtualY();
         
         int coordenadaAntigaX = pessoa.getCoordenadaAntigaX();
         int coordenadaAntigaY = pessoa.getCoordenadaAntigaY();
-        
+           
         int numeroDaCor = pessoa.getNumeroDaCor();
         
-        mapa[coordenadaAtualY][coordenadaAtualX] = numeroDaCor;
-        mapa[coordenadaAntigaY][coordenadaAntigaX] = 0;
+        mapaFisico[coordenadaAtualY][coordenadaAtualX] = numeroDaCor;
+        mapaFisico[coordenadaAntigaY][coordenadaAntigaX] = 0;
+        
+        String whatsAppID = pessoa.getWhatsAppID();
+        
+        mapaDados[coordenadaAtualY][coordenadaAtualX] = whatsAppID;
+        mapaDados[coordenadaAntigaY][coordenadaAntigaX] = "0";
+    }
+    public void movimentaPessoas(){       
+        
+        for(Pessoa pessoa: pessoasDoMundo){
+            pessoa.mover();  
+            atualizaPessoasNosMapas(pessoa);            
+        }
+        
+    }
+    public void verificarEncontroDePessoas(Pessoa pessoa){
+  
+    }   
+    
+    public void animacaoMundo(){
+        
+        while(true){
+            
+            desenhaMundoConsole();
+            movimentaPessoas();
+            
+            try{
+                Thread.sleep(500);
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+            
+        }
         
     }
     
     public Mundo(){
-        
-        this.geraMatrizMundo();
-        
-        //TODO adicionar todas as classes que irao compor a classe mundo
-       
-        
+        this.gerarMatrizMundo();    
+        this.gerarPessoasMundo();
+        //TODO adicionar todas as classes que irao compor a classe mundo  
     }
 
 }
