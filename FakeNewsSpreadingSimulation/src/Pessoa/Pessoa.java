06e44e0b011caccaf9edf65ce570c4479da61b76
brunoandwele <bruno.3a@outlook.com>
2023-04-20 13:28:21 -0300

    Classes criadas de acordo com a UML base

---
+++ FakeNewsSpreadingSimulation/src/Pessoa/Pessoa.java
@@ -0,0 +1,21 @@
+package Pessoa;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author bruno
+ */
+
+public abstract class Pessoa {
+    
+    private int coordenadaX, coordenadaY;
+    
+    private int numeroDaCor;
+    
+    private String whatsAppID;
+    
+    private ArrayList<String> contatos = new ArrayList<String>();
+    
+    
+}
